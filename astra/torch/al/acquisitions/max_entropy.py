519fc6dc884dcb03c4fa4ed8c0859a807966afdd
rishabh-mondal <rishabh.mondal19@gmail.com>
2023-10-29 00:36:25 +0530

    max entropy acquasition and test case

--- astra/torch/al/acquisitions/max_entropy.py
+++ astra/torch/al/acquisitions/max_entropy.py
@@ -4,29 +4,13 @@ from base import EnsembleAcquisition, MCAcquisition
 class MaxEntropy(EnsembleAcquisition, MCAcquisition):
     def acquire_scores(self, logits: torch.Tensor):
         
-        #this is ensemble strategy
+        #calculate entropy for each pool datapoint for each model
+        probs=torch.softmax(logits,dim=2)
+        entropy=-torch.sum(probs*torch.log(probs),dim=2)
 
 
-        if isinstance(self,EnsembleAcquisition):
-            #calculate entropy for each pool datapoint for each model
-            probs=torch.softmax(logits,dim=2)
-            entropy=-torch.sum(probs*torch.log(probs),dim=2)
+        score=torch.sum(entropy,dim=0)
 
+        return score
 
-            score=torch.sum(entropy,dim=0)
-
-            return score
-        
-        elif isinstance(self,MCAcquisition):
-            #calculate entropy for each pool datapoint accross all Monte Carlo samples  
-            probs = torch.softmax(logits, dim=2)
-            
-            entropy=-torch.sum(probs*torch.log(probs),dim=2)
-
-            score=torch.mean(entropy,dim=0)
-
-            return score
-        
-        else:
-            raise NotImplementedError("Unknown acquisition strategy")
       
