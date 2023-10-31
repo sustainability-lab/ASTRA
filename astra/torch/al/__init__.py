0ea28b5a7cabbc3dc107a3fe95301e175d3ff31c
patel-zeel <patel_zeel@iitgn.ac.in>
2023-10-31 16:28:54 +0530

    fix test

--- astra/torch/al/__init__.py
+++ astra/torch/al/__init__.py
@@ -1,9 +1,10 @@
 # Strategies
 from astra.torch.al.strategies.base import Strategy
-from astra.torch.al.strategies.random import RandomStrategy
+from astra.torch.al.strategies.deterministic import DeterministicStrategy
+from astra.torch.al.strategies.diversity import DiversityStrategy
 from astra.torch.al.strategies.ensemble import EnsembleStrategy
 from astra.torch.al.strategies.mc import MCStrategy
-from astra.torch.al.strategies.diversity import DiversityStrategy
+from astra.torch.al.strategies.random import RandomStrategy
 
 # Acquisition base classes
 from astra.torch.al.acquisitions.base import (
@@ -12,6 +13,7 @@ from astra.torch.al.acquisitions.base import (
     MCAcquisition,
     EnsembleAcquisition,
     DiversityAcquisition,
+    DeterministicAcquisition,
 )
 
 # Acquisition functions
--- tests/torch/acquisitions/test_common.py
+++ tests/torch/acquisitions/test_common.py
@@ -10,17 +10,28 @@ from astra.torch.al import (
     MCAcquisition,
     DiversityStrategy,
     DiversityAcquisition,
+    DeterministicAcquisition,
+    DeterministicStrategy,
 )
 from astra.torch.al.errors import AcquisitionMismatchError
 
 
-def test_mutual_pairs():
-    acquision = UniformRandomAcquisition()
+def test_acquisition_match():
     inputs = torch.rand(10, 3, 32, 32)
     outputs = torch.randint(0, 10, (10,))
+    acquision = UniformRandomAcquisition()
     strategy = RandomStrategy(acquision, inputs, outputs)
+    # If code reaches here, then the test passes
 
-    class DummyAcquisition(EnsembleAcquisition):
+
+@pytest.mark.parametrize(
+    "another_class", [EnsembleAcquisition, MCAcquisition, DiversityAcquisition, DeterministicAcquisition]
+)
+def test_acquisition_mismatch(another_class):
+    inputs = torch.rand(10, 3, 32, 32)
+    outputs = torch.randint(0, 10, (10,))
+
+    class DummyAcquisition(another_class):
         def acquire_scores(self, logits):
             return logits
 
