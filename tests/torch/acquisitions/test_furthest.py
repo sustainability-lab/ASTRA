fc04b4d9c473de0d44f509b44bec9fc4a88602e6
jaiswalsuraj47 <jaiswalsuraj@iitgn.ac.in>
2023-11-04 22:46:54 +0530

    updated featurizer

--- tests/torch/acquisitions/test_furthest.py
+++ tests/torch/acquisitions/test_furthest.py
@@ -1,7 +1,7 @@
 import torch
 from torchvision.datasets import CIFAR10
 
-from astra.torch.models import CNN
+from astra.torch.models import CNNClassifier
 
 # from astra.torch.al.acquisitions.furthest import acquire_scores
 from astra.torch.al import Furthest, DiversityStrategy
@@ -24,29 +24,16 @@ def test_furthest():
     # Define the acquisition function
     acquisition = Furthest()
     strategy = DiversityStrategy(acquisition, inputs, outputs)
+    
     # Put the strategy on the device
     strategy.to(device)
+    
     # Define the model
-    net = CNN(32, 3, 3, [4, 8], [2, 3], 10).to(device)
+    net = CNNClassifier(32, 3, 3, [4, 8], [2, 3], 10).to(device)
 
-    def extract_features(net):
-        def feature_extractor(input_tensor):
-            # Initialize features with the input tensor
-            features = input_tensor
-
-            # Apply each layer, activation, and max-pooling
-            for layer in net.feature_extractor:
-                features = layer(features)
-                features = net.activation(features)
-                features = net.max_pool(features)
-            features = net.flatten(features)
-            return features
-
-        return feature_extractor
-
-    # Create a feature extractor callable from the network
-    feature_extractor = extract_features(net)
-
+    # Feature extractor callable from the network
+    feature_extractor = net.featurizer
+    
     # Query the strategy
     best_indices = strategy.query(
         feature_extractor, pool_indices, train_indices, n_query_samples=n_query_samples
